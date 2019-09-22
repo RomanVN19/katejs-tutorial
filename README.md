dd3ce4ee9323438e7f47c6de5c9ec0d6dd787835
romannep <neproman@gmail.com>
2019-09-22 13:17:19 +0400

    upd to 1.0.19

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Boilerplate app for [KateJS](https://github.com/romannep/katejs)
+
+
+# Database
+По умолчанию используется база данных MySQL
+
+Для использования SQLite
+````
+npm install sqlite3
+````
+`env[.local].json`
+````
+{
+  "database": { 
+    "dialect": "sqlite",
+    "storage": "./test.db"
+  },
+}
+````
--- src/server.js
+++ src/server.js
@@ -15,7 +15,11 @@ const database = {
   database: process.env.DB_DB || env.database.database,
   username: process.env.DB_USER || env.database.username,
   password: process.env.DB_PASSWORD || env.database.password,
+  storage: process.env.DB_STORAGE || env.database.storage,
 };
+if (env.database.dialect) {
+  database.dialect = env.database.dialect;
+}
 const http = {
   port: process.env.PORT || env.http.port,
 };
