b58f6116752f69ad756e6b26ee6acee1f18ad9d8
romannep <neproman@gmail.com>
2019-09-23 20:42:31 +0400

    1. Base

--- src/AppClient.js
+++ src/AppClient.js
@@ -1,7 +1,5 @@
 import { use } from 'katejs/lib/client';
 
-import TestForm from 'forms/TestForm';
-
 import { structures, title, packageName } from './structure';
 import env from './front.env.json';
 
@@ -16,19 +14,8 @@ const AppClient = parent => class Client extends use(parent) {
 
     this.forms = {
       ...this.forms,
-      TestForm,
     };
 
-    this.menu.push({
-      form: 'TestForm',
-      title: 'Test form',
-    });
-    // this.forms contains:
-    //   TaskList,
-    //   TaskItem,
-    //
-    // Forms classes can be changed by form name
-    // this.forms[_form_name_]
   }
 };
 AppClient.package = packageName;
