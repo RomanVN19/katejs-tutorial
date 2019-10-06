9bd1aeb9c50efcd757583af32d4778fb8dc6769d
romannep <neproman@gmail.com>
2019-10-06 15:58:35 +0400

    5. Money

--- src/AppServer.js
+++ src/AppServer.js
@@ -1,9 +1,11 @@
 import { makeEntitiesFromStructures, use } from 'katejs';
+import { AppDoc } from 'katejs-modules';
 import { structures, title, packageName } from './structure';
 
 import ExpenseMixin from './entities/ExpenseMixin';
+import IncomeMixin from './entities/IncomeMixin';
 
-const AppServer = parent => class Server extends use(parent) {
+const AppServer = parent => class Server extends use(parent, AppDoc) {
   static title = title;
 
   constructor(params) {
@@ -13,7 +15,9 @@ const AppServer = parent => class Server extends use(parent) {
     this.entities = {
       ...this.entities,
       Expense: ExpenseMixin(this.entities.Expense),
+      Income: IncomeMixin(this.entities.Income),
     };
+    this.entities.MoneyRecord.record = true;
   }
 };
 AppServer.package = packageName;
